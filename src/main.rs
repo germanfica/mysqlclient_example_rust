09d84ef5aadeabd3b2a6195a01b5ecda06148430
German Fica <germafica@gmail.com>
2024-08-30 00:41:39 -0300

    Add db connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+extern crate mysqlclient_sys as ffi;
+use dotenv::dotenv;
+use std::ffi::{CStr, CString};
+use std::{env, ptr};
+
 fn main() {
-    println!("Hello, world!");
+    // Load environment varibles from .env file
+    dotenv().ok();
+
+    // Get environment varibles
+    let db_host = env::var("DB_HOST").expect("DB_HOST must be set");
+    let db_user = env::var("DB_USERNAME").expect("DB_USERNAME must be set");
+    let db_pass = env::var("DB_PASSWORD").expect("DB_PASSWORD must be set");
+    let db_name = env::var("DB_NAME").expect("DB_NAME must be set");
+    let db_port: u32 = env::var("DB_PORT")
+        .expect("DB_PORT must be set")
+        .parse()
+        .expect("DB_PORT must be a number");
+
+    unsafe {
+        // Init MySQL
+        let mysql = ffi::mysql_init(ptr::null_mut());
+        if mysql.is_null() {
+            eprintln!("Error initializing MySQL");
+            return;
+        }
+
+        // Connect to the db
+        let host = CString::new(db_host).unwrap();
+        let user = CString::new(db_user).unwrap();
+        let pass = CString::new(db_pass).unwrap();
+        let dbname = CString::new(db_name).unwrap();
+        let port = db_port;
+
+        if ffi::mysql_real_connect(
+            mysql,
+            host.as_ptr(),
+            user.as_ptr(),
+            pass.as_ptr(),
+            dbname.as_ptr(),
+            port,
+            ptr::null(),
+            0,
+        )
+        .is_null()
+        {
+            eprintln!(
+                "Error connecting to the database: {}",
+                CStr::from_ptr(ffi::mysql_error(mysql)).to_str().unwrap()
+            );
+            ffi::mysql_close(mysql);
+            return;
+        }
+
+        println!("Successfully connected to the database!");
+
+        // Close connection
+        ffi::mysql_close(mysql);
+    }
 }
